0e89ef1df9965fef4b5a73dfd145f2c4940ce694
RyanHate <ryanrongalerios494@gmail.com>
2024-11-06 11:39:30 +0800

    3parts in edit 11/6/2024

--- pages/Users/userData/ctrl.edit.users.php
+++ pages/Users/userData/ctrl.edit.users.php
@@ -15,7 +15,7 @@ if (isset($_POST['upload'])){
         header("location: ../edit.users.php?user_id=". $user_id);
     }
 
-} elseif (isset($_POST['submit'])) {
+} elseif (isset($_POST['update'])) {
 
     $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
     $middlename = mysqli_real_escape_string($conn, $_POST['middlename']);
@@ -28,18 +28,17 @@ if (isset($_POST['upload'])){
     $address = mysqli_real_escape_string($conn, $_POST['address']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $contact = mysqli_real_escape_string($conn, $_POST['contact']);
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $select_user = mysqli_query($conn, "SELECT * FROM tbl_users WHERE username = '$username'
+
+    $select_user = mysqli_query($conn, "SELECT * FROM tbl_users WHERE email = '$email'
     AND user_id NOT IN ('$user_id')");
     $check = mysqli_num_rows($select_user);
 
     if ($check == 0) {
         $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
         $insert_data = mysqli_query($conn, "UPDATE tbl_users SET firstname = '$firstname', middlename = '$middlename',
-        lastname = '$lastname', role_id = '$role', campus_id = '$campus', gender_id = '$gender', civil_id = '$civilstat', birthdate = '$birthdate', address = '$address', email = '$email', contact = '$contact',
-        username = '$username', password = '$hashed_pass' WHERE user_id = '$user_id'");
+        lastname = '$lastname', role_id = '$role', campus_id = '$campus', gender_id = '$gender', civil_id = '$civilstat', birthdate = '$birthdate', address = '$address', email = '$email', contact = '$contact'
+        WHERE user_id = '$user_id'");
         $_SESSION['updated'] = true;
         header("location: ../edit.users.php?user_id=". $user_id);
 
@@ -48,6 +47,25 @@ if (isset($_POST['upload'])){
         $_SESSION['error_update'] = true;
         header("location: ../edit.users.php?user_id=". $user_id);
 
+    }
+} elseif (isset($_POST['submit'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $select_user = mysqli_query($conn, "SELECT * FROM tbl_users WHERE username = '$username' 
+    AND user_id NOT IN ('$user_id')");
+    $check = mysqli_num_rows($select_user);
+
+    if ($check == 0) {
+        $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+        $insert_data = mysqli_query($conn, "UPDATE tbl_users SET username = '$username', password = '$hashed_pass'
+        WHERE user_id = '$user_id'");
+        $_SESSION['update'] = true;
+        header("location: ../edit.users.php?user_id=". $user_id);
+
+    } else {
+        $_SESSION['error_update'] = true;
+        header("location: ../edit.users.php?user_id=". $user_id);   
     }
 }
 
